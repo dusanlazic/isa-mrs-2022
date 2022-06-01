7b85ea7a66ede4ecedccebfaede321d677c264dd
Milan Sekulic <sekulic.sw54.2019@uns.ac.rs>
2022-06-01 14:01:59 +0200

    Added conditional rendering to optional features in AboutTab

--- client/src/components/profile/additional/AboutTab.js
+++ client/src/components/profile/additional/AboutTab.js
@@ -4,25 +4,30 @@ const About = ({data}) => {
 	let rules = data.rules.split(/\r?\n/);
 
 	return (
-		<div className="text-left text-gray-600">
+		<div className="flex flex-col gap-y-10 text-left text-gray-600">
 
-			<div className="mb-5 text-xl text-black">Optional features</div>
-			<div className="grid grid-cols-1">
-				{data.options.map((x, index) =>
-				{
-					return (
-						<span key={index} className="mb-2">&#8729; {x.name} ({x.maxCount}) - {x.description}</span>
-					)
-				}
-				)}
-			</div>
+      { data.options.length > 0 &&
+      <div>
+        <div className="mb-5 text-xl text-black">Optional features</div>
+        <div className="grid grid-cols-1">
+          {data.options.map((x, index) =>
+          {
+            return (
+              <span key={index} className="mb-2">&#8729; {x.name} ({x.maxCount}) - {x.description}</span>
+            )
+          }
+          )}
+        </div>
+      </div>}
 
-			<div className="mt-10 mb-5 text-xl text-black">Rules</div>
-			<div className="grid grid-cols-1">
-				{rules.map((x, index) =>
-					<span key={index} className="mb-2">&#8729; {x}</span>
-				)}
-			</div>
+			<div>
+        <div className="mb-4 text-xl text-black">Rules</div>
+        <div className="grid grid-cols-1">
+          {rules.map((x, index) =>
+            <span key={index} className="mb-2">&#8729; {x}</span>
+          )}
+        </div>
+      </div>
 		</div>
 	);
 }

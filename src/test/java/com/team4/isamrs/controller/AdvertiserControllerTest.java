3ace2b0371e7bf9d54e6f6e4a93eb9b4658d1e7a
Milan Sekulic <milansekkulic@gmail.com>
2022-06-22 05:51:02 +0200

    Add AuthenticationController test

--- src/test/java/com/team4/isamrs/controller/AdvertiserControllerTest.java
+++ src/test/java/com/team4/isamrs/controller/AdvertiserControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
--- src/test/java/com/team4/isamrs/controller/CustomerControllerTest.java
+++ src/test/java/com/team4/isamrs/controller/CustomerControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -60,6 +61,4 @@ public class CustomerControllerTest {
                 .andExpect(jsonPath("$.[*].comment").value(hasItem("you've got mail")))
                 .andExpect(jsonPath("$.[*].customer.firstName").value(hasItem(CustomerConstants.DB_FIRST_NAME)));
     }
-
-
 }

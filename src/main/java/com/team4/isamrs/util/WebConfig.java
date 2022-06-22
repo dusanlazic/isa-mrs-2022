22b638b015635cf7e4d18835fd6aa0150db8452a
Dušan Lazić <lazicdusan1104@gmail.com>
2022-06-22 02:03:42 +0200

    Add 127.0.0.1 to CORS configuration

--- src/main/java/com/team4/isamrs/util/WebConfig.java
+++ src/main/java/com/team4/isamrs/util/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins(
+                "http://127.0.0.1:3000",
                 "http://localhost:3000",
                 "http://www.pecaj.ga",
                 "https://pecaj-ga-frontend.herokuapp.com");

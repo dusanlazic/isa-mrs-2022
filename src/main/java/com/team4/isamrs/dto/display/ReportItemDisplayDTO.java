3038168f98eac322dcac22f956c8b6eca3817d15
anaandjelic <ana.andjelic161@gmail.com>
2022-06-22 22:24:53 +0200

    Create endpoints for graphs

---
+++ src/main/java/com/team4/isamrs/dto/display/ReportItemDisplayDTO.java
@@ -0,0 +1,9 @@
+package com.team4.isamrs.dto.display;
+
+import lombok.Data;
+
+@Data
+public class ReportItemDisplayDTO implements DisplayDTO {
+    private String name;
+    private Integer value;
+}

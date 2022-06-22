7470f6ff17f03b1aed6c7b97e07182dc88e32e57
Milan Sekulic <milansekkulic@gmail.com>
2022-06-22 23:38:57 +0200

    Add 'conditional rendering' to client reservation modal options part

--- client/src/components/modals/reservation/CustomerReservationModal.js
+++ client/src/components/modals/reservation/CustomerReservationModal.js
@@ -150,41 +150,44 @@ const CustomerReservationModal = ({data, close}) => {
             </div>
 
             {/* options */}
-            <div className="text-xs text-slate-500 mt-4 mb-2">Options:</div>
-            <div className="flex flex-col divide-y font-display h-68 overflow-auto">
-              {data.options.map(option => 
-                <div key={option.id} className="py-1">
-                  {option.maxCount === 1 &&
-                    <div className="flex gap-x-2">
-                      <input type="checkbox" className="my-auto w-10 h-4 accent-cyan-700"
-                      onChange={(e) => handleSetOption(option, e.target.value)}
-                      checked={Object.keys(options).length > 0 ? options[option.id] : ""}/>
-
-                      <div className="block">
-                        <p>{option.name}</p>
-                        <div className="text-xs font-display text-slate-700">{option.description}</div>
-                      </div>
-                    </div>
-                  }
-                  {option.maxCount > 1 &&
-                    <div className="flex w-full gap-x-2">
-                      <input type="number" placeholder={option.name}
-                      value={Object.keys(options).length > 0 ? options[option.id] : ""}
+            { Object.keys(options).length > 0 &&
+            <div>
+              <div className="text-xs text-slate-500 mt-4 mb-2">Options:</div>
+              <div className="flex flex-col divide-y font-display h-68 overflow-auto">
+                {data.options.map(option => 
+                  <div key={option.id} className="py-1">
+                    {option.maxCount === 1 &&
+                      <div className="flex gap-x-2">
+                        <input type="checkbox" className="my-auto w-10 h-4 accent-cyan-700"
                         onChange={(e) => handleSetOption(option, e.target.value)}
-                        className={`block rounded-lg px-2.5 text-center border text-gray-700 border-gray-300 py-1
-                        focus:outline-none focus:border-gray-500 w-10 caret-gray-700 shrink h-10
-                        ${options[option.id] > 0 ? 'border-cyan-700 border-2' : ''}`}/>
+                        checked={Object.keys(options).length > 0 ? options[option.id] : ""}/>
 
                         <div className="block">
-                          <p className="my-auto break-all">{option.name} (max {option.maxCount})</p>
-                          <div className={`text-xs font-display text-slate-700
-                          ${option.description.length === 0 ? 'hidden' : 'block'}`}>{option.description}</div>
+                          <p>{option.name}</p>
+                          <div className="text-xs font-display text-slate-700">{option.description}</div>
                         </div>
-                    </div>
-                  }
-                </div>
-                )}
-            </div>
+                      </div>
+                    }
+                    {option.maxCount > 1 &&
+                      <div className="flex w-full gap-x-2">
+                        <input type="number" placeholder={option.name}
+                        value={Object.keys(options).length > 0 ? options[option.id] : ""}
+                          onChange={(e) => handleSetOption(option, e.target.value)}
+                          className={`block rounded-lg px-2.5 text-center border text-gray-700 border-gray-300 py-1
+                          focus:outline-none focus:border-gray-500 w-10 caret-gray-700 shrink h-10
+                          ${options[option.id] > 0 ? 'border-cyan-700 border-2' : ''}`}/>
+
+                          <div className="block">
+                            <p className="my-auto break-all">{option.name} (max {option.maxCount})</p>
+                            <div className={`text-xs font-display text-slate-700
+                            ${option.description.length === 0 ? 'hidden' : 'block'}`}>{option.description}</div>
+                          </div>
+                      </div>
+                    }
+                  </div>
+                  )}
+              </div>
+            </div>}
 
             <div>
               <button className="rounded-xl shadow-sm px-6 py-2 text-white font-display font-bold 
